e94214b6f25d661ad88bd1c6af045d7251baab77
mmhancxt <mmhancxt@gmail.com>
2015-03-12 22:34:13 +0100

    4ee

--- src/llxd/FullRandom.java
+++ src/llxd/FullRandom.java
@@ -1,5 +1,11 @@
 package llxd;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Vector;
+
 public class FullRandom {
 
 	DataCenter dc;
@@ -9,6 +15,21 @@ public class FullRandom {
 	}
 	
 	public void RunRandom() {
+		System.out.println("Run here");
+		
+		for (int i=0;i<dc.getServerList().size();i++) {
+			Random r = new Random();
+			int low = 0;
+			int high = 45;
+			int pullId = r.nextInt(high - low) + low;
+			Server s = dc.getServerList().get(i);
+			s.SetPoolId(pullId);
+			if (s.row == -1) {
+				System.out.println("x");
+			} else {
+				System.out.println(s.row + " " + s.slot + " " + pullId);
+			}
+		}
 		
 	}
 }
