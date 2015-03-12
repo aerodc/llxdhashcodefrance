56c2c72cfdb2d65251d3cb9ba413b5a2bf46c895
mmhancxt <mmhancxt@gmail.com>
2015-03-12 19:39:44 +0100

    1

--- src/llxd/Test.java
+++ src/llxd/Test.java
@@ -1,7 +1,41 @@
 package llxd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Test {
-   public static void main(String[] args) {
-      System.out.println("We are the champions!");
+	
+	
+   public static void main(String[] args) {      
+		DataCenter dc = new DataCenter(16, 100);
+		ReadData(dc);
+   }
+   
+   public static void ReadData(DataCenter dc) {
+	   BufferedReader br = null;
+	   
+		try {
+
+			String line;
+
+			br = new BufferedReader(new FileReader("dc.in"));
+
+			line = br.readLine();
+			while ((line = br.readLine()) != null) {
+				String[] strs = line.split(" ");
+				
+			}
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
    }
 }
